230be880a96a45e6cf86c5f0f93e96c356ffbfb2
ronatskiy <r.onatskiy@gmail.com>
2018-05-12 23:20:06 +0300

    Move chevron icon to common components

---
+++ src/components/common/chevron-icon.js
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+import FontAwesomeIcon from "@fortawesome/react-fontawesome";
+import { faAngleDown } from "@fortawesome/fontawesome-free-solid";
+import cn from "classnames";
+
+import "./chevron-icon.css";
+
+const ChevronIcon = ({ direction = "down", className = "" }) => {
+	const classNames = cn("chevron-icon", `chevron-icon--${direction}`, className);
+
+	return <FontAwesomeIcon className={classNames} icon={faAngleDown} />;
+};
+
+ChevronIcon.propTypes = {
+	className: PropTypes.string,
+	direction: PropTypes.oneOf(["down", "top", "left", "right"]),
+};
+
+export default ChevronIcon;
