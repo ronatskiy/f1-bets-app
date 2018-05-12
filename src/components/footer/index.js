f2e7b2ccf09fd2c0c526a0adca5a520c8b53e28c
ronatskiy <r.onatskiy@gmail.com>
2018-05-13 00:17:53 +0300

    Optimize bundle size

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import FontAwesomeIcon from "@fortawesome/react-fontawesome";
-import { faTelegram } from "@fortawesome/fontawesome-free-brands";
+import * as faTelegram from "@fortawesome/fontawesome-free-brands/faTelegram";
 
 import "./styles.css";
 
