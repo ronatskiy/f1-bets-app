af52337335aab2540fa0d52807e0e528b5c9d4cd
ronatskiy <r.onatskiy@gmail.com>
2019-03-15 18:40:25 +0200

    Updates

---
+++ precache-manifest.ace59a0c21b3d66a26bd0d1ba1a5bc88.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "f231fc97eb438f66fb87",
+    "url": "/static/css/6.02e77146.chunk.css"
+  },
+  {
+    "revision": "4aa92055f982a45c7c64",
+    "url": "/static/js/0.4aa92055.chunk.js"
+  },
+  {
+    "revision": "fb04af633073c7e93e00",
+    "url": "/static/js/main.fb04af63.chunk.js"
+  },
+  {
+    "revision": "6f43a39ed0eb2bcea1808bd6ba60bbb1",
+    "url": "/static/media/chevron.6f43a39e.svg"
+  },
+  {
+    "revision": "f69e2cdd314ef8d3c24d",
+    "url": "/static/js/2.f69e2cdd.chunk.js"
+  },
+  {
+    "revision": "8a14cb4c0b9fa8d771832e5f1170b1bb",
+    "url": "/static/media/telegram.8a14cb4c.svg"
+  },
+  {
+    "revision": "d98d9a313efb90d07ded",
+    "url": "/static/js/3.d98d9a31.chunk.js"
+  },
+  {
+    "revision": "1d21cecaf6f9ae9a0a92",
+    "url": "/static/js/runtime~main.1d21ceca.js"
+  },
+  {
+    "revision": "49e90b09bbe210f5faa6",
+    "url": "/static/js/4.49e90b09.chunk.js"
+  },
+  {
+    "revision": "e5baf51d9c6ea3cb7891",
+    "url": "/static/js/9.e5baf51d.chunk.js"
+  },
+  {
+    "revision": "84783a1ab5bffe1664be",
+    "url": "/static/js/5.84783a1a.chunk.js"
+  },
+  {
+    "revision": "c74925135d3e8243ff77",
+    "url": "/static/js/8.c7492513.chunk.js"
+  },
+  {
+    "revision": "f231fc97eb438f66fb87",
+    "url": "/static/js/6.f231fc97.chunk.js"
+  },
+  {
+    "revision": "4067b03650210af45d1d",
+    "url": "/static/js/7.4067b036.chunk.js"
+  },
+  {
+    "revision": "fb04af633073c7e93e00",
+    "url": "/static/css/main.644d8ba3.chunk.css"
+  },
+  {
+    "revision": "e5baf51d9c6ea3cb7891",
+    "url": "/static/css/9.e4bb7980.chunk.css"
+  },
+  {
+    "revision": "c74925135d3e8243ff77",
+    "url": "/static/css/8.da80ac1c.chunk.css"
+  },
+  {
+    "revision": "4067b03650210af45d1d",
+    "url": "/static/css/7.a73ad99e.chunk.css"
+  },
+  {
+    "revision": "84783a1ab5bffe1664be",
+    "url": "/static/css/5.665cdf23.chunk.css"
+  },
+  {
+    "revision": "49e90b09bbe210f5faa6",
+    "url": "/static/css/4.65ae9cc6.chunk.css"
+  },
+  {
+    "revision": "d98d9a313efb90d07ded",
+    "url": "/static/css/3.65dcec1e.chunk.css"
+  },
+  {
+    "revision": "f69e2cdd314ef8d3c24d",
+    "url": "/static/css/2.42801ec9.chunk.css"
+  },
+  {
+    "revision": "d5caabe88d730270e112dc5f51520b4f",
+    "url": "/index.html"
+  }
+];
